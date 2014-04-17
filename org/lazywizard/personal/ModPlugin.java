fb9f81f537e5902554432d60f0701bb231baf1e3
LazyWizard <iu65a@hotmail.com>
2014-04-17 06:32:41 -0500

    LocalMP: Bugfixes, refactoring, started work on new left axis control scheme

--- org/lazywizard/personal/ModPlugin.java
+++ org/lazywizard/personal/ModPlugin.java
@@ -9,22 +9,23 @@ import com.fs.starfarer.api.combat.ShipAIPlugin;
 import com.fs.starfarer.api.combat.ShipAPI;
 import com.fs.starfarer.api.fleet.FleetMemberAPI;
 import org.apache.log4j.Level;
-import org.lazywizard.localmp.JoypadController;
+import org.lazywizard.localmp.JoypadControls;
 
 public class ModPlugin extends BaseModPlugin
 {
+    private static final boolean OVERRIDE_ALL_AI = false;
+
     @Override
     public PluginPick<ShipAIPlugin> pickShipAI(FleetMemberAPI member, ShipAPI ship)
     {
         CombatEngineAPI engine = Global.getCombatEngine();
-        if ((member != null && member.isFlagship())
-                || (engine != null && ship == engine.getPlayerShip()))
+        if (OVERRIDE_ALL_AI || (engine != null && ship == engine.getPlayerShip()))
         {
             Global.getLogger(ModPlugin.class).log(Level.INFO,
                     "Setting joypad controls to true for "
                     + ship.getHullSpec().getHullName());
-            return new PluginPick<ShipAIPlugin>(new JoypadController(ship),
-                    PickPriority.MOD_SET);
+            return new PluginPick<ShipAIPlugin>(new JoypadControls(ship),
+                    PickPriority.HIGHEST);
         }
 
         return null;
